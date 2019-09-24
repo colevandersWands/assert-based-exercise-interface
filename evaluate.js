afef479e176830a6de9efa0605e6a223c0bf4bce
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-09-24 16:17:33 +0200

    better-object-comparison

--- evaluate.js
+++ evaluate.js
@@ -160,9 +160,7 @@ const evaluate = (() => {
   evaluate.compareValues = (returned, expected) => {
     let areTheSame;
     if (typeof expected === 'object' && expected !== null) {
-      const _returned = JSON.stringify(returned);
-      const _expected = JSON.stringify(expected);
-      areTheSame = _returned === _expected;
+      areTheSame = evaluate.isEqualObjects(returned, expected);
     } else if (expected !== expected) {
       areTheSame = returned !== returned;
     } else {
@@ -170,6 +168,54 @@ const evaluate = (() => {
     }
     return areTheSame;
   }
+
+  // https://gomakethings.com/check-if-two-arrays-or-objects-are-equal-with-javascript/
+  evaluate.isEqualObjects = (value, other) => {
+
+    var type = Object.prototype.toString.call(value);
+
+    if (type !== Object.prototype.toString.call(other)) return false;
+
+    if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+    var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+    var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+    if (valueLen !== otherLen) return false;
+
+    var compare = function (item1, item2) {
+
+      var itemType = Object.prototype.toString.call(item1);
+
+      if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+        if (!isEqual(item1, item2)) return false;
+      }
+      else {
+        if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+        if (itemType === '[object Function]') {
+          if (item1.toString() !== item2.toString()) return false;
+        } else {
+          if (item1 !== item2) return false;
+        }
+      }
+    };
+
+    if (type === '[object Array]') {
+      for (var i = 0; i < valueLen; i++) {
+        if (compare(value[i], other[i]) === false) return false;
+      }
+    } else {
+      for (var key in value) {
+        if (value.hasOwnProperty(key)) {
+          if (compare(value[key], other[key]) === false) return false;
+        }
+      }
+    }
+
+    return true;
+
+  };
+
 
   evaluate.assessImplementation = (func, isNative, args) => {
 
